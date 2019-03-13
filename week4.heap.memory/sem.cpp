d845f47f0eafe5933b949034d2680d7fea5a43ef
NickGeorgiev <nikikg95@gmail.com>
2019-03-13 22:09:27 +0200

    Added wip version of the code from seminars

---
+++ week4.heap.memory/sem.cpp
@@ -0,0 +1,99 @@
+#include<iostream>
+#include<cstring>
+
+//we will use 
+class Student;
+//predicate type function in the filter method below
+using predicate = bool (*) (const Student&);
+
+//mapping function in the mapOperation below
+using mapper = void (*) (Student&);
+
+
+class Student{
+    size_t facultyNumber;
+    float avg_grades;
+    char* name;
+
+    //we moved the logic from the copy-constructor into a separate function in order to use it in other methods if needed
+    void copy(const Student& other){
+        facultyNumber = other.facultyNumber;
+        avg_grades = other.avg_grades;
+        name = new char[strlen(other.name)+1];
+        strcpy(name, other.name);
+    }
+
+    void destroy(){
+        delete [] name;
+    }
+
+    public:
+
+    Student() {
+        facultyNumber = 71000;
+        avg_grades = 3.25;
+        name = new char[6]; // new char[strlen("Petar")+1]
+        strcpy(name, "Petar");
+    }
+
+    Student(const Student& other) {
+        copy(other);
+    }
+
+    Student( int fn, float grades, char* name):facultyNumber(fn), avg_grades(grades) {
+        this->name = new char[strlen(name) + 1];
+        strcpy(this->name, name);
+    }
+
+    Student& operator = (const Student& other) {
+        if(this != &other){
+            destroy();
+            copy(other);
+        }
+        return *this;
+    }
+
+    ~Student(){
+        destroy();
+    }
+
+    size_t getFn() const {
+        return this->facultyNumber;
+    }
+
+    float getGrades() const {
+        return this->avg_grades;
+    }
+
+    const char* getName() const {
+        return this->name;
+    }
+
+    void setFn(size_t facultyNumber) {
+        this->facultyNumber = facultyNumber; 
+    }
+
+    void setGrades(float newGrades) {
+        this->avg_grades = newGrades;
+    }
+
+    void seName(const char* newName) {
+        strcpy(this->name, newName);
+    }
+
+    void print() const {
+        std::cout << this->facultyNumber << '\n';
+        std::cout << this->name << '\n';
+        std::cout << this->avg_grades << '\n';
+    }
+};
+
+int main(){
+
+    Student st;
+    st.print();
+    Student st2(st);
+    // Student test("Ivan Petrov Ivanov");
+
+    return 0;
+}
